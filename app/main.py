6e54e6f5f8191e088d58988e4e838060a462d77e
adityasahirrao <adityasahirrao@gmail.com>
2025-04-08 21:30:39 +0530

    minor change

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ class HouseFeaturesBatch(BaseModel):
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to the House Price Prediction API"}
+    return {"message": "Welcome to the House Price Prediction API : latest"}
 
 
 @app.get("/health")
